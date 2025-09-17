d9cad618c6aff422125895eeba26854c20b60fd8
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-09-17 09:35:36 -0400

    Completed exercises 06 and 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arr = str.split('')
 
+    let rev = arr.reverse()
+
+    let result = rev.join('')
+    
+    return (result);
 };
+
+
+console.log(reverseString('hello good morning'));
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
