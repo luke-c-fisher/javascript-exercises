fc230f9fedd6eafaf5cec3c4e20791223682784f
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-09-20 10:01:34 -0400

    Complete exercise 9, sumAll

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, out) {
+    let index = array.indexOf(out)
+    if (index > -1){
+        array.splice(index, 1); 
+    }
+    return array;
 };
+
+const arr = [1,2,5,6]
+
+console.log(removeFromArray(arr, 2));
+
+// The significance of the conditional, if (index > -1), is to ensure that the indexOf method doesn't remove the last element of the
+// array, as it does so when it doesn't recognize the element it's given. 
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+// The goal of this exercise is to create a function that takes (1) two parameters, (2) find the range between the two paramets,
+// and (3) sums up the values in the range, inclusive of the two parameters. 
 
+const sumAll = function(a, b) {
+    let arr = []
+    // Convert parameters into an array to iterate over
+    for (let i = a; i <= b; i++){ 
+        arr.push(i)
+    }
+    return arr.reduce((sum, current) => sum + current);
+
+    // Iterate over array to add each element to each other  
 };
+
+// === IMPROVED CODE ===
+const sumAll = function(a, b) {
+
+    // Test to ensure that the arguments passed through this function are integers; otherwise, an error is returned
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"; 
+
+    // Ensures that a or b are non-negative
+    if (a < 0 || b < 0) return "ERROR";
+
+    // If a is greater than b, we can ensure that they're swapped for each other
+    if (a > b) {
+    const temp = a;
+    a = b;
+    b = temp;
+  }
+
+    
+    let arr = []
+    // Convert parameters into an array to iterate over
+    for (let i = a; i <= b; i++){ 
+        arr.push(i)
+    }
+    return arr.reduce((sum, current) => sum + current);
+
+    // Iterate over array to add each element to each other  
+};
+
+
+
+console.log(sumAll('hi', 4));
 
 // Do not edit below this line
 module.exports = sumAll;
