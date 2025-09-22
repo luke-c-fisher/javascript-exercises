10efb1e654e2a5cda21c1fcddf45d862b01b3fde
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-09-22 10:08:04 -0400

    Complete exercises 10 and 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const initialTemp = temp
+  
+  let toCelsius = (initialTemp - 32) * (5/9);
+  let roundedTemp = Number(toCelsius.toFixed(1));
+
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const initialTemp = temp
+  
+  let toFahrenheit = (initialTemp * (9/5)) + 32;
+  let roundedTemp = Number(toFahrenheit.toFixed(1));
+
+  return roundedTemp;
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(36))
 
 // Do not edit below this line
 module.exports = {
