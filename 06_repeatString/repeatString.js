f9c80852a9f404430a12f49bacba88fb9d4e0f83
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-09-22 10:10:41 -0400

    Include edits to leapYears and repeatString js files

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    for (let i = 0; i < num ; i++) {
+        console.log(string)
+    }
 };
+
+repeatString('string', 6)
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR"; 
+    if (year < 0) return "ERROR";
+
+    if (year % 100 == 0 && year % 400 !== 0 || year % 4 !== 0) return false;
+    if (year % 4 == 0) return true;
 
 };
+
+
+console.log(leapYears('hello world'));
+console.log(leapYears(1800));
+console.log(leapYears(1600));
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
