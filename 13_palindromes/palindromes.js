587ff029863b20f5caaa37e501f7613fd1aec782
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-15 18:56:15 -0400

    Add revString and newString variables to palindromes function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.replace(/[^A-Z0-9]/ig, "");
+    let revString = string.split("").reverse().join("");
+    return (revString == newString);
+}
 
-};
+// https://stackoverflow.com/questions/13020246/remove-special-symbols-and-extra-spaces-and-replace-with-underscore-using-the-re
 
+console.log(palindromes('A car, a man, a maraca.'));
+// console.log(palindromes('tacos'));
+
+
+// .split(" ").reverse().join(" ")
 // Do not edit below this line
 module.exports = palindromes;
