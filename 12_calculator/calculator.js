3c59263dd57c0bcd2ce1a53a16719c8c32927d86
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-15 18:16:21 -0400

    Finish calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,29 +1,49 @@
 const add = function(a, b) {
 	let sum = a + b; 
-  return sum
+  return sum;
 };
 
 console.log(add(1, 6));
 
-const subtract = function() {
+const subtract = function(a, b) {
+  let result = b - a;
+  return result;
 	
 };
 
-const sum = function() {
-	
+console.log(subtract(1, 6));
+
+const sum = function(arr) {
+  let sum = arr.reduce((a, b) => a + b, 0);
+	return sum;
 };
 
-const multiply = function() {
+console.log(sum([1, 5, 6, 9]));
+
+const multiply = function(arr) {
+  let product = arr.reduce((a, b) => a * b);
+  return product;
+};
+
+console.log(multiply([5, 6, 3]));
+
+const power = function(a, b) {
+  let result = a ** b; 
+  return result
+};
+
+console.log(power(4, 3));
+
+const factorial = function(n) {
+  let result = 1
+  for (let i = 1; i <= n; i++){
+    result *= i;
+  }
+  return result;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
