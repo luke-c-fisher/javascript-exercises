1052b732e73866b4c30ca8ad726f2343362e9714
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-16 10:16:32 -0400

    Add an empty array to the fibonacci function to hold the results of the fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++){
+        arr += i;
+    }
+    return arr;
 
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
