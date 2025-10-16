d8f316b0a103a243a670fbbf2eca6d2374f59e4a
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-16 12:01:45 -0400

    Complete fibonacci function by including an inital array, a loop to iterate over it and perform the fibonacci sequence, and a return statement that returns the fibonacci number for the respective index.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,13 +1,30 @@
-const fibonacci = function(num) {
-    let arr = [];
-    for (let i = 1; i <= num; i++){
-        arr += i;
-    }
-    return arr;
 
+
+
+// for (const [i, num] of arr.entries()){
+//     console.log(`${i}:${num}`);
+// }
+
+// for (let i = 1; i <= arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+
+// the role of 'num' is to index that through the fibonacci sequence
+// and return the respective value at that index (i.e., index of 4 = 3)
+
+const fibonacci = function(num) {
+    if (num <= 0) return null;
+
+    const fib = [1, 1];
+    for (let i = 2; i < num; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[num - 1];
 };
 
-console.log(fibonacci(10));
+console.log(fibonacci(90));
 
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
