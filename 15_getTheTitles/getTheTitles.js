7081d5d04ac9095912adfe43ea347a3d35593707
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-17 11:04:53 -0400

    Complete getTheTitles exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,27 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    // return arr.reduce((a,b) => {
+    //     return [a.title, b.title];
+    // });
+    return arr.map((book) => book.title);
 };
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  },
+   {
+    title: 'Book3',
+    author: 'Name2'
+  },
+
+]
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
