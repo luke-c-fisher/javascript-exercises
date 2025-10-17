5b63c829aa935763bbdf563cfa1f5db28c3c13f6
Luke Fisher <162048027+luke-c-fisher@users.noreply.github.com>
2025-10-17 11:46:38 -0400

    Create first draft of findTheOldest function using a basic .reduce() function call iterating through array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((accum, current) => {
+        accum[current[0]] = current[1];
+        return accum;
+    })
+    // return array.sort((a, b) => b.yearOfDeath - a.yearOfDeath);
 };
+
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
